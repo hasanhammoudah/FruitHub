d17825c522238a31f0cc501c4e17ef241e30210e
Hasan Hammoudah <hassun.h98@gmail.com>
2024-09-06 18:18:45 +0300

    NEW UPDATE

--- lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
@@ -4,6 +4,7 @@ import 'package:fruits_hub/core/helper_functions/build_error_bar.dart';
 import 'package:fruits_hub/core/widgets/custom_progress_hud.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:fruits_hub/features/home/presentation/views/home_view.dart';
 
 class SigninViewBodyBlocConsumer extends StatelessWidget {
   const SigninViewBodyBlocConsumer({
@@ -14,7 +15,9 @@ class SigninViewBodyBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<SigninCubit, SigninState>(
       listener: (context, state) {
-        if (state is SigninSuccess) {}
+        if (state is SigninSuccess) {
+          Navigator.pushNamed(context, HomeView.routeName);
+        }
         if (state is SigninFailure) {
           buildErrorBar(context, state.message);
         }
---
+++ lib/features/home/presentation/views/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+  static const routeName = "home_view";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
